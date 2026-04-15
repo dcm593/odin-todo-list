2d4366b554039e0bf2a34ec5b63d80f5cf5d0bc1
Damon Martin-Durocher <73200348+dcm593@users.noreply.github.com>
2026-04-15 15:37:34 -0600

    Setup Todo Factory
    Handles creation of a todo list item that contains a title, description, due date, priority, and notes section. Will also be able to toggle if the item has been completed.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 import "./styles.css";
+
+const createTodo = (title, description, dueDate, priority, notes = "") => {
+    let completed = false;
+    
+    const toggleCompleted = () => {
+        completed = !completed;
+    };
+
+    const setNotes = (newNotes) => {
+        notes = newNotes;
+    };
+
+    const getData = () => {
+        return {
+            title,
+            description,
+            dueDate,
+            priority,
+            notes,
+            completed
+        };
+    };
+
+    return {
+        toggleCompleted,
+        setNotes,
+        getData
+    };
+};
+
+// just verifying that js is running via webpack server
+console.log("Hello, World!");
